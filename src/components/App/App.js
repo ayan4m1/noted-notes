567910399cd33090bb4c805cd42b84c37646e97a
ayan4m1 <andrew@bulletlogic.com>
2019-12-29 09:32:55 -0500

    add DataManager component for save/load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
+import DataManager from 'components/DataManager/DataManager';
 import NoteTable from 'components/NoteTable/NoteTable';
 
 export class App extends Component {
   render() {
-    return <NoteTable />;
+    return (
+      <Fragment>
+        <DataManager />
+        <NoteTable />
+      </Fragment>
+    );
   }
 }
 
