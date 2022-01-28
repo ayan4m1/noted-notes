a13f36ccc88eeaa5352fc2726bce119539711330
ayan4m1 <andrew@bulletlogic.com>
2022-01-28 14:59:24 -0500

    begin refactor of class to functional components

--- src/components/AddRow/AddRow.js
+++ src/components/AddRow/AddRow.js
@@ -1,86 +1,60 @@
 // eslint-disable-next-line import/no-unresolved
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useCallback, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default class AddRow extends Component {
-  static propTypes = {
-    onAdd: PropTypes.func.isRequired
-  };
+export default function AddRow({ onAdd }) {
+  const [flavor, setFlavor] = useState('');
+  const [notes, setNotes] = useState('');
 
-  constructor(props) {
-    super(props);
-
-    this.handleAddClick = this.handleAddClick.bind(this);
-    this.handleFlavorChange = this.handleFlavorChange.bind(this);
-    this.handleNotesChange = this.handleNotesChange.bind(this);
-    this.state = {
-      flavor: '',
-      notes: ''
-    };
-  }
-
-  handleAddClick() {
-    const { flavor, notes } = this.state;
-    const { onAdd } = this.props;
-
+  const handleAddClick = useCallback(() => {
     onAdd({
       flavor,
       id: nanoid(),
       notes
     });
 
-    this.setState({
-      flavor: '',
-      notes: ''
-    });
-  }
+    setFlavor('');
+    setNotes('');
+  }, [onAdd, setFlavor, setNotes]);
+  const handleFlavorChange = useCallback(
+    ({ target: { value } }) => setFlavor(value),
+    [setFlavor]
+  );
+  const handleNotesChange = useCallback(
+    ({ target: { value } }) => setNotes(value),
+    [setNotes]
+  );
 
-  handleFlavorChange(event) {
-    const {
-      target: { value: flavor }
-    } = event;
-
-    this.setState({ flavor });
-  }
-
-  handleNotesChange(event) {
-    const {
-      target: { value: notes }
-    } = event;
-
-    this.setState({ notes });
-  }
-
-  render() {
-    const { flavor, notes } = this.state;
-
-    return (
-      <tr>
-        <td></td>
-        <td className="text-center">
-          <Button variant="success" onClick={this.handleAddClick}>
-            <FontAwesomeIcon icon="plus-circle" />
-          </Button>
-        </td>
-        <td>
-          <Form.Control
-            type="text"
-            value={flavor}
-            onChange={this.handleFlavorChange}
-          />
-        </td>
-        <td>
-          <Form.Control
-            as="textarea"
-            value={notes}
-            onChange={this.handleNotesChange}
-            rows={1}
-          />
-        </td>
-      </tr>
-    );
-  }
+  return (
+    <tr>
+      <td></td>
+      <td className="text-center">
+        <Button variant="success" onClick={handleAddClick}>
+          <FontAwesomeIcon icon="plus-circle" />
+        </Button>
+      </td>
+      <td>
+        <Form.Control
+          type="text"
+          value={flavor}
+          onChange={handleFlavorChange}
+        />
+      </td>
+      <td>
+        <Form.Control
+          as="textarea"
+          value={notes}
+          onChange={handleNotesChange}
+          rows={1}
+        />
+      </td>
+    </tr>
+  );
 }
+
+AddRow.propTypes = {
+  onAdd: PropTypes.func.isRequired
+};
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,55 +1,36 @@
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Component } from 'react';
-import { bindActionCreators } from 'redux';
+import { useDispatch } from 'react-redux';
 import { Nav, Navbar } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import ImportNavLink from 'components/ImportNavLink/ImportNavLink';
 import { actions as applicationActions } from 'reducers/application';
 
-class Header extends Component {
-  static propTypes = {
-    actions: PropTypes.shape({
-      dataClear: PropTypes.func.isRequired,
-      dataExport: PropTypes.func.isRequired,
-      dataImport: PropTypes.func.isRequired,
-      dataLoad: PropTypes.func.isRequired,
-      dataSave: PropTypes.func.isRequired
-    }).isRequired
-  };
+export default function Header() {
+  const dispatch = useDispatch();
 
-  render() {
-    const { actions } = this.props;
-
-    return (
-      <Navbar bg="primary" variant="dark" expand="lg">
-        <Navbar.Brand className="mx-4">Noted Notes</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Nav>
-            <Nav.Link onClick={actions.dataLoad}>
-              <FontAwesomeIcon icon="spinner" fixedWidth /> Load
-            </Nav.Link>
-            <Nav.Link onClick={actions.dataSave}>
-              <FontAwesomeIcon icon="save" fixedWidth /> Save
-            </Nav.Link>
-            <ImportNavLink dataImport={actions.dataImport} />
-            <Nav.Link onClick={actions.dataExport}>
-              <FontAwesomeIcon icon="file-download" fixedWidth /> Export
-            </Nav.Link>
-            <Nav.Link onClick={actions.dataClear}>
-              <FontAwesomeIcon icon="trash" fixedWidth /> Clear
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand className="mx-4">Noted Notes</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link onClick={() => dispatch(applicationActions.dataLoad())}>
+            <FontAwesomeIcon icon="spinner" fixedWidth /> Load
+          </Nav.Link>
+          <Nav.Link onClick={() => dispatch(applicationActions.dataSave())}>
+            <FontAwesomeIcon icon="save" fixedWidth /> Save
+          </Nav.Link>
+          <ImportNavLink
+            dataImport={(data) => dispatch(applicationActions.dataImport(data))}
+          />
+          <Nav.Link onClick={() => dispatch(applicationActions.dataExport())}>
+            <FontAwesomeIcon icon="file-download" fixedWidth /> Export
+          </Nav.Link>
+          <Nav.Link onClick={() => dispatch(applicationActions.dataClear())}>
+            <FontAwesomeIcon icon="trash" fixedWidth /> Clear
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
-
-export const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(applicationActions, dispatch)
-});
-
-export default connect(null, mapDispatchToProps)(Header);
